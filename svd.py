6f5ca39934337b421e0132eff006a85fcd112645
Arush Kharbanda <kharbandarush@gmail.com>
2018-06-12 16:42:43 +0530

    testing

--- svd.py
+++ svd.py
@@ -3,11 +3,11 @@
 
 
 #Benchmarking Setup - Configure here for the benchmark size required
-sizes=[100,1000, 10000, 50000, 100000, 500000, 1000000]
-#sizes=[100,1000]
+#sizes=[100,1000, 10000, 50000, 100000, 500000, 1000000]
+sizes=[100,1000]
 
-cores=[1,2,4,8,16,32,64,128]
-#cores=[1,2,4,8]
+#cores=[1,2,4,8,16,32,64,128]
+cores=[1,2,4,8]
 
 #Refer Assumption 1, change this if changing AWS instance type
 cores_on_single_machine=4
@@ -18,6 +18,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.mllib.linalg.distributed import RowMatrix
 from datetime import datetime
 from pyspark.mllib.random import RandomRDDs
+from pyspark.mllib.linalg import Vectors
 
 benchmarks={}
 
@@ -30,25 +31,29 @@ def print_metrics(benchmarks):
 def g(x):
     print x
 
+def h(x, a):
+    a[x._1] = x._2
+
 
 def transposeRowMatrix(m):
-    m.rows.foreach(lambda  x: g(x))
-    transposedRowsRDD = m.rows.zipWithIndex().foreach(g)
+    transposedRowsRDD = m.rows.zipWithIndex().map(lambda x:  rowToTransposedTriplet(x._1, x._2)).flatmap(lambda x:x).groupByKey().sortByKey().map(lambda  x:x._2).map(lambda x: buildRow(x))
     #.rows.zipWithIndex.map{case (row, rowIndex) => rowToTransposedTriplet(row, rowIndex)}.flatMap(x => x).groupByKey.sortByKey().map(_._2).map(buildRow)
-    RowMatrix(m)
-    #RowMatrix(transposedRowsRDD)
 
-'''
+    RowMatrix(transposedRowsRDD)
+
+
 def rowToTransposedTriplet(row, rowIndex):
-    indexedRow = row.toArray.zipWithIndex
-    indexedRow.map{case (value, colIndex) => (colIndex.toLong, (rowIndex, value))}
+    indexedRow = row.toArray.zipWithIndex()
+    indexedRow.map(lambda x: (long(x._2), (rowIndex, x._1)) )
+    #indexedRow.map{case (value, colIndex) => (colIndex.toLong, (rowIndex, value))}
 
 
 def buildRow(rowWithIndexes):
-    resArr = new Array[Double](rowWithIndexes.size)
-    rowWithIndexes.foreach{case (index, value) =>resArr(index.toInt) = value}
+    resArr = rowWithIndexes.size
+    rowWithIndexes.foreach(lambda x: h(x,resArr) )
+    #rowWithIndexes.foreach{case (index, value) =>resArr(index.toInt) = value}
     Vectors.dense(resArr)
-'''
+
 
 
 #Generate a array and requset it for all core configuration
@@ -71,7 +76,7 @@ for size in sizes:
         randomRdd = RandomRDDs.normalRDD(sc, size)
         randomRdd.foreach(lambda x: g(x))
         m=RowMatrix(randomRdd)
-        #transposeRowMatrix(m)
+        transposeRowMatrix(m)
 
 
 '''
