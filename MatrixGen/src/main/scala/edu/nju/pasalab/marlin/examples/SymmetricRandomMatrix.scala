f333fe4051dd2a981bf721f06ebe711bba7eaa1b
Arush Kharbanda <kharbandarush@gmail.com>
2018-06-13 13:26:37 +0530

    aws try

--- MatrixGen/src/main/scala/edu/nju/pasalab/marlin/examples/SymmetricRandomMatrix.scala
+++ MatrixGen/src/main/scala/edu/nju/pasalab/marlin/examples/SymmetricRandomMatrix.scala
@@ -22,21 +22,12 @@ object SymmetricRandomMatrix {
     //val sizes=Array (100,1000, 10000, 50000, 100000, 500000, 1000000)
 
     val conf = new  SparkConf().setAppName("SVD-Datagen").set("spark.executor.cores", "4").set("spark.executor.instances", "32" ).setMaster("local[2]")
-      //.set("spark.executor.extraJavaOptions","-Dcom.amazonaws.services.s3.enableV4=true")
-      //.set("spark.driver.extraJavaOptions","-Dcom.amazonaws.services.s3.enableV4=true")
       .set("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version","2")
       .set("spark.speculation","false")
     val sc = new SparkContext(conf)
     sc.hadoopConfiguration.set("fs.s3.awsAccessKeyId", System.getenv("AWS_ACCESS_KEY"))
     sc.hadoopConfiguration.set("fs.s3.awsSecretAccessKey", System.getenv("AWS_SECRET_ACCESS_KEY"))
     sc.hadoopConfiguration.set("fs.s3.impl", "org.apache.hadoop.fs.s3.S3FileSystem")
-    /*sc.hadoopConfiguration.set("fs.s3a.awsAccessKeyId", "AKIAJNOJTH2RA4QO6MTA")
-    sc.hadoopConfiguration.set("fs.s3a.awsSecretAccessKey", "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")
-    sc.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", "AKIAJNOJTH2RA4QO6MTA")
-    sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", "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")
-    sc.hadoopConfiguration.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")*/
-
 
     for(size <- sizes)
       {
